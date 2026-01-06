3b463d3784631505e5f90615eddcd678b6f1afac
xactly406 <zacktgill@gmail.com>
2026-01-05 20:41:29 -0700

    added player movement

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
-        dt = (clock.tick()) / 1000
+        dt = (clock.tick(60)) / 1000
 
 if __name__ == "__main__":
     main()
