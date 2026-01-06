a2ea1de276224c5085e34b9618b415c94d77902f
xactly406 <zacktgill@gmail.com>
2026-01-05 19:46:35 -0700

    changed fps

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -18,6 +20,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = (clock.tick()) / 1000
 
 if __name__ == "__main__":
     main()
